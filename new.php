deb131ff629d35bfc6de4f299651fedaee5e0096
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-07-01 11:40:19 +0700

    Update and rename new to new.php

--- new.php
+++ new.php
@@ -1,4 +1,5 @@
 <?php
+
 require "vendor/autoload.php";
 $access_token = 'yQw5mqImEwMHcau8Hb9CXnPQaTlz11cUCGhUZL64yG1GyAyMJddLMqfjiLwlZgvKfdC2yo896ykJVwW8Xne9++3BjCqj9xsNEdeENjtWVda5UTFIw149B2ygMnCp/4Fcn/nAV1YYOX1YLNxEJkiHwwdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '5c1c1cb6d2769f4783b63c8ddddd8f82';
@@ -9,3 +10,4 @@ $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello
 $response = $bot->pushMessage('U7ef7a449f2a5c2057eacfc02ba2eb286', $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+>
