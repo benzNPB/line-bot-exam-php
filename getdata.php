aa91b4976142ee91d6a34de87dd37432151f4a26
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-07-15 20:45:14 +0700

    Update getdata.php

--- getdata.php
+++ getdata.php
@@ -8,7 +8,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id : " . $row["iddb"]. " - lat: " . $row["lati"]. "  long : " . $row["longt"]. "<br>";
+        echo "number : " . $row["iddb"]. " - lat: " . $row["lati"]. "  long : " . $row["longt"]. "<br>";
     }
 } else {
     echo "results";
