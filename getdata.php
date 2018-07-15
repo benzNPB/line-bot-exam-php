a81cff037185c032ef1ed182d0f9c0450d0e054b
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-07-15 20:40:57 +0700

    Update getdata.php

--- getdata.php
+++ getdata.php
@@ -2,13 +2,13 @@
 
 require "dbconnection.php";
 
-$sql = "SELECT iddb, lat, long FROM db";
+$sql = "SELECT iddb, lati, longt FROM db";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id : " . $row["iddb"]. " - lat: " . $row["lat"]. "  long : " . $row["long"]. "<br>";
+        echo "id : " . $row["iddb"]. " - lat: " . $row["lati"]. "  long : " . $row["longt"]. "<br>";
     }
 } else {
     echo "results";
