fb3ac2b6c8a4eb011b365ba6cab5114ec9e1c14c
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-07-15 13:29:13 +0700

    Add files via upload

--- getdata.php
+++ getdata.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id : " . $row["iddb"]. " - status: " . $row["status"]. " - stat : " . $row["stat"]. "<br>";
+        echo "id : " . $row["iddb"]. " - status: " . $row["status"]. "  stat : " . $row["stat"]. "<br>";
     }
 } else {
     echo "0 results";
