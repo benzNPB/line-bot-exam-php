d2b1395a5e25c51523f4759cd63d8e0629c53c07
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-07-15 20:16:56 +0700

    Update getdata.php

--- getdata.php
+++ getdata.php
@@ -5,7 +5,7 @@ require "dbconnection.php";
 $sql = "SELECT iddb, lat, long FROM db";
 $result = $conn->query($sql);
 
-if ($result->num_rows = 0) {
+if ($result->num_rows < 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "id : " . $row["iddb"]. " - lat: " . $row["lat"]. "  long : " . $row["long"]. "<br>";
