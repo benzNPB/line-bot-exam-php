1f1beb3abf3aaf611baf54557e4d8f1d1ac8a591
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-07-15 16:20:48 +0700

    Add files via upload

--- getdata.php
+++ getdata.php
@@ -2,13 +2,13 @@
 
 require "dbconnection.php";
 
-$sql = "SELECT iddb, status, stat FROM db";
+$sql = "SELECT iddb, data1, data2 FROM db";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id : " . $row["iddb"]. " - status: " . $row["status"]. "  stat : " . $row["stat"]. "<br>";
+        echo "id : " . $row["iddb"]. " - data1: " . $row["data1"]. "  data2 : " . $row["data2"]. "<br>";
     }
 } else {
     echo "results";
