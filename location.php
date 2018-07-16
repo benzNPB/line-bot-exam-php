6f9f87a523b6fe80bef0b991be32a517568624ed
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-07-16 09:48:37 +0700

    Update location.php

--- location.php
+++ location.php
@@ -8,7 +8,7 @@
     $arrayHeader[] = "Authorization: Bearer {$accessToken}";
     
     //รับข้อความจากผู้ใช้
-  //  $message = $arrayJson['events'][0]['message']['text'];
+$message = $arrayJson['events'][0]['message']['text'];
 #ตัวอย่าง Message Type "Text"
       if($message == "พิกัด"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
