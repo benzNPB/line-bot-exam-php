54d0a06dfb214db2ffb8b5f433a3647e92a8465b
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-07-16 10:53:40 +0700

    Add files via upload

--- getdataupdate.php
+++ getdataupdate.php
@@ -1,3 +1,4 @@
+
 <?php
 require "dbconnection.php";
 $sql = "SELECT iddb FROM db";
@@ -6,7 +7,7 @@ if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc())
      {
-        echo "id : " . $row["iddb"] "<br>";
+        echo "id : " . $row["iddb"] " "<br>";
     }
 } else {
     echo "results";
