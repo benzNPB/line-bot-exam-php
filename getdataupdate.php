603680fa2c487a08ca2c43a50448258c224ba0bf
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-07-16 11:20:37 +0700

    Add files via upload

--- getdataupdate.php
+++ getdataupdate.php
@@ -1,12 +1,13 @@
 <?php
 require "dbconnection.php";
-$sql = "SELECT iddb, lati, longt FROM db";
+$sql = "SELECT max(iddb), lati, longt FROM db";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
+
     while($row = $result->fetch_assoc())
-    {
-        echo "id : " . $row["iddb"]. "<br>";
+     {
+        echo " - lat: " . $row["lati"]. "  long : " . $row["longt"]. "<br>";
     }
 } else
 {
