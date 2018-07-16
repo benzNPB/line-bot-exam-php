a2dd6d90be45d4507f1407161382611d2e78751e
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-07-16 10:54:42 +0700

    Update getdataupdate.php

--- getdataupdate.php
+++ getdataupdate.php
@@ -7,7 +7,7 @@ if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc())
      {
-        echo "id : " . $row["iddb"] " "<br>";
+        echo "id : " . $row["iddb"] "<br>";
     }
 } else {
     echo "results";
