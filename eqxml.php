147106c9edcb2a73f63e9b5c0c9175dc54e758f9
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-07-23 11:22:54 +0700

    Update eqxml.php

--- eqxml.php
+++ eqxml.php
@@ -1,8 +1,8 @@
 <?php
 $url = "http://www.earthquake.tmd.go.th/feed/rss_inside.xml";
 $xml = simplexml_load_file($url);
-echo $xml;
-//echo $xml->channel->title;
+
+echo $xml->channel->title;
 //echo $xml->channel->item[1]->title;
 //$o = strpos($xml->channel->item[1]->title,"(" );
 //$c = strpos($xml->channel->item[1]->title,")" );
