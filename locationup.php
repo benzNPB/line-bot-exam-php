4cc38d9fecbd3fc828cc187b33941222f58f3f20
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-07-23 11:49:05 +0700

    Update locationup.php

--- locationup.php
+++ locationup.php
@@ -32,11 +32,11 @@ $lo = substr($xml->channel->item[0]->title,$s+1 ,$c-$s+1);
            $arrayPostData['messages'][0]['latitude'] = $row["lati"];
            $arrayPostData['messages'][0]['longitude'] =$row["longt"];
             
-           $arrayPostData['messages'][0]['type'] = "location";
-           $arrayPostData['messages'][0]['title'] = "equake";
-           $arrayPostData['messages'][0]['address'] = $row["la"].",".$row["lo"];
-           $arrayPostData['messages'][0]['latitude'] = $row["la"];
-           $arrayPostData['messages'][0]['longitude'] =$row["lo"];
+           $arrayPostData['messages'][1]['type'] = "location";
+           $arrayPostData['messages'][1]['title'] = "equake";
+           $arrayPostData['messages'][1]['address'] = $row["la"].",".$row["lo"];
+           $arrayPostData['messages'][1]['latitude'] = $row["la"];
+           $arrayPostData['messages'][1]['longitude'] =$row["lo"];
         }else{
           $arrayPostData['messages'][0]['type'] = "text";
           $arrayPostData['messages'][0]['text'] = "error";
