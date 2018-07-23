a4dbd62cd8eda576094c2458c00007d8320dcd5d
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-07-23 10:52:30 +0700

    Update eqxml.php

--- eqxml.php
+++ eqxml.php
@@ -4,7 +4,7 @@ $xml = simplexml_load_file($url);
 echo $xml->channel->title;
 echo $xml->item[1]->title;
 //echo $xml->item->[1]->title;
-echo "<br><br><br>";
+//echo "<br><br><br>";
 print_r($xml);
 
 ?>
