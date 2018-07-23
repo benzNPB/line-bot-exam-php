524f228804f1ecdd363b1b36f777fa0efac6ffa8
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-07-23 12:06:27 +0700

    Update eqxml.php

--- eqxml.php
+++ eqxml.php
@@ -4,6 +4,8 @@ $xml = simplexml_load_file($url);
 //echo $xml->channel->title;
 //echo $xml->channel->item[1]->title;
 $o = strpos($xml->channel->item[0]->title,"(" );
+$s = strpos($xml->channel->item[0]->title,"," );
 $c = strpos($xml->channel->item[0]->title,")" );
-echo substr($xml->channel->item[0]->title,$o+1 ,$c-$o-1);
+echo substr($xml->channel->item[0]->title,$o+1 ,$s-$o-1);
+echo substr($xml->channel->item[0]->title,$s+1 ,$c-$s-1);
 ?>
