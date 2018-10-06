5101b64d6b474c23dbd49a159292c876908c86f2
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-10-06 19:25:12 +0700

    Update testdis.php

--- testdis.php
+++ testdis.php
@@ -4,10 +4,10 @@ $long1 = 139.269988;
 $lat2 = 35.372896;
 $long2 =139.270909;
 $R = 6,371;
-$Δlat = $lat1 - $lat2;
-$Δlong = $long1 - $long2;
-$a = sin²($Δlat /2) + cos($lat1)*cos($lat2)*sin²($Δlong/2);
-$c = 2*$a*tan2(√$a, √(1−$a));
+$lat = $lat1 - $lat2;
+$long = $long1 - $long2;
+$a = sin²($lat /2) + cos($lat1)*cos($lat2)*sin²($long/2);
+$c = 2*$a*tan2(sqrt($a), sqrt(1 − $a));
 $d = $R*$c
-echo d;
+echo $d;
 ?>
