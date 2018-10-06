619ea3446938d57482c0088bc691cf54b82c7e02
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-10-06 20:32:51 +0700

    Add files via upload

--- testdis.php
+++ testdis.php
@@ -1,14 +1,17 @@
 <?php
 
-$lat1 = 35.372331;
-$long1 = 139.269988;
-$lat2 = 35.372896;
-$long2 =139.270909;
-$R = 6371;
-$Δlat = $lat1 - $lat2;
-$Δlong = $long1 - $long2;
-$a = sin²($Δlat /2) + cos($lat1)*cos($lat2)*sin²($Δlong/2);
-$c = 2*$a*tan2(√$a, √(1−$a));
-$d = $R*$c;
-echo $d
+	$lat1 = 35.372331;
+	$long1 = 139.269988;
+	$lat2 = 35.372896;
+	$long2 =139.270909;
+	$R = 6371;
+    
+    $deltaLat = deg2rad($lat2 - $lat1);
+    $deltaLong = deg2rad($long2 - $long1);
+
+    $a = sin($deltaLat/2) * sin($deltaLat/2) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin($deltaLong/2) * sin($deltaLong/2);
+    $c = 2 * atan2(sqrt($a), sqrt(1-$a));
+
+    $distance = $R * $c;
+    return $distance;    // in km
 ?>
