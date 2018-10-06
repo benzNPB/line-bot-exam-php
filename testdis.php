09a901bc59b77633932f6440dd43ec1c25701871
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-10-06 19:45:21 +0700

    Update testdis.php

--- testdis.php
+++ testdis.php
@@ -3,7 +3,7 @@ $lat1 = 35.372331;
 $long1 = 139.269988;
 $lat2 = 35.372896;
 $long2 = 139.270909;
-$R = 6,371;
+//$R = 6,371;
 echo "Hello LINE BOT";
 //$lat = $lat1 - $lat2;
 //$long = $long1 - $long2;
