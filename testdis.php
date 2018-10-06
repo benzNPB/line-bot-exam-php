957bd94f1209124ab002b224c01439f34bdf5a4d
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-10-06 20:05:04 +0700

    Add files via upload

--- testdis.php
+++ testdis.php
@@ -1,14 +1,14 @@
-<?php		
+<?php
+
 $lat1 = 35.372331;
 $long1 = 139.269988;
 $lat2 = 35.372896;
-$long2 = 139.270909;
+$long2 =139.270909;
 $R = 6371;
-echo "Hello LINE BOT";
-$lat = $lat1 - $lat2;
-$long = $long1 - $long2;
-$a = sin($lat /2)+ cos($lat1) * cos($lat2) * sin($long/2);
-$c = 2 * $a * tan2(sqrt($a), sqrt(1 − $a));
-$d = $R*$c
-echo $lat1;
+$Δlat = $lat1 - $lat2;
+$Δlong = $long1 - $long2;
+$a = sin²($Δlat /2) + cos($lat1)*cos($lat2)*sin²($Δlong/2);
+$c = 2*$a*tan2(√$a, √(1−$a));
+$d = $R*$c;
+echo $d
 ?>
