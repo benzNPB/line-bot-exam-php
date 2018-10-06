b1778ecdc454f68b7cd5907b5944e50b0150eb0d
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-10-06 19:37:46 +0700

    Update testdis.php

--- testdis.php
+++ testdis.php
@@ -5,8 +5,8 @@ $lat2 = 35.372896;
 $long2 = 139.270909;
 $R = 6,371;
 echo "Hello LINE BOT";
-$lat = $lat1 - $lat2;
-$long = $long1 - $long2;
+//$lat = $lat1 - $lat2;
+//$long = $long1 - $long2;
 //$a = sin($lat /2)+ cos($lat1) * cos($lat2) * sin($long/2);
 //$c = 2*$a*tan2(sqrt($a), sqrt(1 − $a));
 //$d = $R*$c
