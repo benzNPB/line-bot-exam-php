19243278410d73edb244ce05e184a78efc822626
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-10-07 14:34:58 +0700

    Update testdis.php

--- testdis.php
+++ testdis.php
@@ -11,10 +11,22 @@
     $deltaLat = deg2rad($lat2 - $lat1);
     $deltaLong = deg2rad($long2 - $long1);
 
+    $deltaLat2 = deg2rad($lat2 - $lat3);
+    $deltaLong2 = deg2rad($long2 - $long3);
+
     $a = sin($deltaLat/2) * sin($deltaLat/2) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin($deltaLong/2) * sin($deltaLong/2);
     $c = 2 * atan2(sqrt($a), sqrt(1-$a));
-
     $distance = $R * $c;
 
+    $a2 = sin($deltaLat2/2) * sin($deltaLat2/2) + cos(deg2rad($lat3)) * cos(deg2rad($lat2)) * sin($deltaLong2/2) * sin($deltaLong2/2);
+    $c2 = 2 * atan2(sqrt($a2), sqrt(1-$a2));
+    $distance2 = $R * $c2;
+
+   echo "distance 1 is "
    echo $distance;    // in km
+   echo "km"
+	   
+   echo "distance 2 is "
+   echo $distance2;    // in km
+   echo "km"
 ?>
