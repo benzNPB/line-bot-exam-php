84be19ab68d51c83005ab31c887d87b32cef1b9b
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-10-21 14:28:13 +0700

    Update testdis.php

--- testdis.php
+++ testdis.php
@@ -26,21 +26,21 @@ $R = 6371;
    $deltaLong5 = deg2rad($longu - $long5);
 
 
-  $a = sin($deltaLat1/2) * sin($deltaLat1/2) + cos(deg2rad($lat1)) * cos(deg2rad($latu)) * sin($deltaLong1/2) * sin($deltaLong1/2);
-  $c = 2 * atan2(sqrt($a), sqrt(1-$a));
-  $dis1 = $R * $c;
+  $a1 = sin($deltaLat1/2) * sin($deltaLat1/2) + cos(deg2rad($lat1)) * cos(deg2rad($latu)) * sin($deltaLong1/2) * sin($deltaLong1/2);
+  $c1 = 2 * atan2(sqrt($a1), sqrt(1-$a1));
+  $dis1 = $R * $c1;
   $a2 = sin($deltaLat2/2) * sin($deltaLat2/2) + cos(deg2rad($lat2)) * cos(deg2rad($latu)) * sin($deltaLong2/2) * sin($deltaLong2/2);
   $c2 = 2 * atan2(sqrt($a2), sqrt(1-$a2));
   $dis2 = $R * $c2;
-  $a = sin($deltaLat3/2) * sin($deltaLat3/2) + cos(deg2rad($lat3)) * cos(deg2rad($latu)) * sin($deltaLong3/2) * sin($deltaLong3/2);
-  $c = 2 * atan2(sqrt($a), sqrt(1-$a));
-  $dis3 = $R * $c;
-  $a2 = sin($deltaLat4/2) * sin($deltaLat4/2) + cos(deg2rad($lat4)) * cos(deg2rad($latu)) * sin($deltaLong4/2) * sin($deltaLong4/2);
-  $c2 = 2 * atan2(sqrt($a2), sqrt(1-$a2));
-  $dis4 = $R * $c2;
+  $a3 = sin($deltaLat3/2) * sin($deltaLat3/2) + cos(deg2rad($lat3)) * cos(deg2rad($latu)) * sin($deltaLong3/2) * sin($deltaLong3/2);
+  $c3 = 2 * atan2(sqrt($a3), sqrt(1-$a3));
+  $dis3 = $R * $c3;
+  $a4 = sin($deltaLat4/2) * sin($deltaLat4/2) + cos(deg2rad($lat4)) * cos(deg2rad($latu)) * sin($deltaLong4/2) * sin($deltaLong4/2);
+  $c4 = 2 * atan2(sqrt($a4), sqrt(1-$a4));
+  $dis4 = $R * $c4;
   $a5 = sin($deltaLat5/2) * sin($deltaLat5/2) + cos(deg2rad($lat5)) * cos(deg2rad($latu)) * sin($deltaLong5/2) * sin($deltaLong5/2);
-  $c5 = 2 * atan2(sqrt($a), sqrt(1-$a));
-  $dis5 = $R * $c;
+  $c5 = 2 * atan2(sqrt($a5), sqrt(1-$a5));
+  $dis5 = $R * $c5;
 
 $dis = min ($dis1,$dis2,$dis3,$dis4,$dis5)
 switch ($dis) {
