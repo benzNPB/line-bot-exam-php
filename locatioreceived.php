1473f5579941e4be00a13c48c985d148c59c5125
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-11-11 15:57:36 +0700

    Update locatioreceived.php

--- locatioreceived.php
+++ locatioreceived.php
@@ -10,7 +10,7 @@
 
     //รับข้อความจากผู้ใช้
     $event = $arrayJson['events'][0]['source']['location'];
-    if ($event = get location) 
+    if ($event == get location) 
 {
           $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
           $arrayPostData['messages'][0]['type'] = "text";
