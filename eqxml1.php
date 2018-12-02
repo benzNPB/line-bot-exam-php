1d547518fc6622b56acdf49d32c8cc085ee20890
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-12-02 22:49:29 +0700

    Update eqxml1.php

--- eqxml1.php
+++ eqxml1.php
@@ -1,6 +1,6 @@
 <?php
 $url = "http://geofon.gfz-potsdam.de/eqinfo/list.php?fmt=rss";
 $xml = simplexml_load_file($url);
-echo $xml->channel->item[0];
+echo $xml->channel->item[0]->description;
 
 ?>
