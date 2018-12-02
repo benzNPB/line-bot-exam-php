bc84eee8f608673cb4ea524a9bcc802f2a2ec73e
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-12-02 22:09:52 +0700

    Update eqxml1.php

--- eqxml1.php
+++ eqxml1.php
@@ -2,6 +2,6 @@
 $url = "http://webcritech.jrc.ec.europa.eu/ModellingTsunami/TsunamiSurge/JETS_System/2018/12/30373/locations.xml";
 $xml = simplexml_load_file($url);
 
-echo $xml->channel->item[4]->title;
+echo $xml->channel->item[0]->title;
 
 ?>
