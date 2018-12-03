fe0826106b5e3e053671db5562b13c73a6514aaa
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-12-03 14:10:13 +0700

    Update eqxml1.php

--- eqxml1.php
+++ eqxml1.php
@@ -7,7 +7,7 @@ $o = strpos($xml->channel->item[0]->description,"2018" );
 $s = strpos($xml->channel->item[0]->description," " );
 $c = strpos($xml->channel->item[0]->description,"km" );
 //echo substr($xml->channel->item[0]->description,$o+16,$c-$s-5);
-echo substr($xml->channel->item[0]->description,$o+20 ,$c-$s-9);
+echo substr($xml->channel->item[0]->description,$o+20 ,$c-$s-15);
 
 
 ?>
