41d2ce51eadf50ab6c59690728ce2963a090c7b8
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-12-09 19:21:53 +0700

    Update eqxml1.php

--- eqxml1.php
+++ eqxml1.php
@@ -1,7 +1,7 @@
 <?php
-$url = "http://www.gdacs.org/xml/rss.xml";
+$url = "http://geofon.gfz-potsdam.de/eqinfo/list.php?fmt=rss";
 $xml1 = simplexml_load_file($url);
-$xml2 = $xml1->channel->item[0]->geo:Point->georss:point;
-
-echo $xml2;
+$xml2 = $xml1->channel->item[0]->description;
+$xml = (explode(" ",$xml2));
+echo $xml[3].','.$xml[6];
 ?>
