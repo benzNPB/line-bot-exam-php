7cedd80defca1fc6c47c7a50b32863bb42bb7b55
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-05-25 20:57:34 +0700

    Update location.php

--- location.php
+++ location.php
@@ -61,25 +61,15 @@
       $result = curl_exec($ch);
       curl_close ($ch);
    }
-function getdata($arrayHeader,$datas)
-{
-      $strUrl = "https://api.line.me/v2/bot/message/{messageId}/content";
-      $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL,$strUrl);
-      curl_setopt($ch, CURLOPT_HEADER, false);
-      curl_setopt($ch, CURLOPT_POST, true);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-      $result = curl_exec($ch);
-      curl_close ($ch);
-        $datas = [];
+
+function getFormatTextMessage($text)
+	{
+		$datas = [];
 		$datas['type'] = 'text';
 		$datas['text'] = $text;
 
 		return $datas;
-}
+	}
  
         
    exit;
