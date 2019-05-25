3cfbb97241ae127431cceb45007730f68042cdb8
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-05-25 15:47:15 +0700

    Update location.php

--- location.php
+++ location.php
@@ -8,7 +8,7 @@
     $arrayHeader[] = "Authorization: Bearer {$accessToken}";
     $text = $arrayJson['events'][0]['message']['text'];
     $location = $arrayJson['events'][0]['message']['location'];
-    $message = $arrayJson;
+    $message = $arrayJson['events'][0]['message']['text'];
 
       if($message == $location)
     {
