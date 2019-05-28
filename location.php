f05b590f4ecfe8ce77cf18490a84ccd13680f536
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-05-28 12:56:06 +0700

    Update location.php

--- location.php
+++ location.php
@@ -23,84 +23,34 @@ $long5 = 139.269099;
 $R = 6371;
     
 
- 
                 
       if($message == $location)
     {
-          $latu = $arrayJson['events'][0]['message']['latitude'];//users location (弘済学園)
-          $longu = $arrayJson['events'][0]['message']['longitude'];
-   $deltaLat1 = deg2rad($lat1 - $latu);
-   $deltaLong1 = deg2rad($long1 - $longu);
-   $deltaLat2 = deg2rad($lat2 - $latu);
-   $deltaLong2 = deg2rad($long2 - $longu);
-   $deltaLat3 = deg2rad($lat3 - $latu);
-   $deltaLong3 = deg2rad($long3 - $longu);
-   $deltaLat4 = deg2rad($lat4 - $latu);
-   $deltaLong4 = deg2rad($long4 - $longu);
-   $deltaLat5 = deg2rad($lat5 - $latu);
-   $deltaLong5 = deg2rad($long5 - $longu);
-  $a1 = sin($deltaLat1/2) * sin($deltaLat1/2) + cos(deg2rad($lat1)) * cos(deg2rad($latu)) * sin($deltaLong1/2) * sin($deltaLong1/2);
+   $latu = $arrayJson['events'][0]['message']['latitude'];//users location 
+   $longu = $arrayJson['events'][0]['message']['longitude'];
+$sql = "SELECT no, lat, long FROM db order by no desc limit 0,1";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+$row = $result->fetch_assoc();
+$deltaLat1 = deg2rad($lat - $latu);
+$deltaLong1 = deg2rad($long - $longu);
+
+  $a1 = sin($deltaLat1/2) * sin($deltaLat1/2) + cos(deg2rad($lat)) * cos(deg2rad($latu)) * sin($deltaLong1/2) * sin($deltaLong1/2);
   $c1 = 2 * atan2(sqrt($a1), sqrt(1-$a1));
   $dis1 = $R * $c1;
-  $a2 = sin($deltaLat2/2) * sin($deltaLat2/2) + cos(deg2rad($lat2)) * cos(deg2rad($latu)) * sin($deltaLong2/2) * sin($deltaLong2/2);
-  $c2 = 2 * atan2(sqrt($a2), sqrt(1-$a2));
-  $dis2 = $R * $c2;
-  $a3 = sin($deltaLat3/2) * sin($deltaLat3/2) + cos(deg2rad($lat3)) * cos(deg2rad($latu)) * sin($deltaLong3/2) * sin($deltaLong3/2);
-  $c3 = 2 * atan2(sqrt($a3), sqrt(1-$a3));
-  $dis3 = $R * $c3;
-  $a4 = sin($deltaLat4/2) * sin($deltaLat4/2) + cos(deg2rad($lat4)) * cos(deg2rad($latu)) * sin($deltaLong4/2) * sin($deltaLong4/2);
-  $c4 = 2 * atan2(sqrt($a4), sqrt(1-$a4));
-  $dis4 = $R * $c4;
-  $a5 = sin($deltaLat5/2) * sin($deltaLat5/2) + cos(deg2rad($lat5)) * cos(deg2rad($latu)) * sin($deltaLong5/2) * sin($deltaLong5/2);
-  $c5 = 2 * atan2(sqrt($a5), sqrt(1-$a5));
-  $dis5 = $R * $c5;
-$dis = min ($dis1,$dis2,$dis3,$dis4,$dis5);
-if ($dis == $dis1) {
+
+
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "location";
-        $arrayPostData['messages'][0]['title'] = "your nearest convenience store";
+        $arrayPostData['messages'][0]['title'] = $dis1;
         $arrayPostData['messages'][0]['address'] =  $lat1 .",". $long1;
         $arrayPostData['messages'][0]['latitude'] =$lat1;
         $arrayPostData['messages'][0]['longitude'] = $long1;
         replyMsg($arrayHeader,$arrayPostData);     
-}
-else if ($dis == $dis2) {
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "location";
-        $arrayPostData['messages'][0]['title'] = "your nearest convenience store";
-        $arrayPostData['messages'][0]['address'] =  $lat2 .",". $long2;
-        $arrayPostData['messages'][0]['latitude'] =$lat2;
-        $arrayPostData['messages'][0]['longitude'] = $long2;
-        replyMsg($arrayHeader,$arrayPostData);     
-}
-else if ($dis == $dis3) {
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "location";
-        $arrayPostData['messages'][0]['title'] = "your nearest convenience store";
-        $arrayPostData['messages'][0]['address'] =  $lat3 .",". $long3;
-        $arrayPostData['messages'][0]['latitude'] =$lat3;
-        $arrayPostData['messages'][0]['longitude'] = $long3;
-        replyMsg($arrayHeader,$arrayPostData);     
-}
-else if ($dis == $dis4) {
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "location";
-        $arrayPostData['messages'][0]['title'] = "your nearest convenience store";
-        $arrayPostData['messages'][0]['address'] =  $lat4 .",". $long4;
-        $arrayPostData['messages'][0]['latitude'] =$lat4;
-        $arrayPostData['messages'][0]['longitude'] = $long4;
-        replyMsg($arrayHeader,$arrayPostData);     
-}
-else if ($dis == $dis5) {
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "location";
-        $arrayPostData['messages'][0]['title'] = "your nearest convenience store";
-        $arrayPostData['messages'][0]['address'] =  $lat5 .",". $long5;
-        $arrayPostData['messages'][0]['latitude'] =$lat5;
-        $arrayPostData['messages'][0]['longitude'] = $long5;
-        replyMsg($arrayHeader,$arrayPostData);     
-}
-     
+
+        }
+
+
     }
 
    else if($message == $text)
