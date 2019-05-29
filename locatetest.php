e1808080ab146c7110ae9ed3256b9126ae3ac40a
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-05-29 14:09:48 +0700

    Update locatetest.php

--- locatetest.php
+++ locatetest.php
@@ -4,6 +4,28 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
    require "dbconnection.php";
+function order_array_num ($array, $key, $order = "ASC") 
+{ 
+	$tmp = array(); 
+	foreach($array as $akey => $array2) 
+	{ 
+		$tmp[$akey] = $array2[$key]; 
+	} 
+	
+	if($order == "DESC") 
+	{arsort($tmp , SORT_NUMERIC );} 
+	else 
+	{asort($tmp , SORT_NUMERIC );} 
+
+	$tmp2 = array();        
+	foreach($tmp as $key => $value) 
+	{ 
+		$tmp2[$key] = $array[$key]; 
+	}        
+	
+	return $tmp2; 
+} 
+
 $accessToken = "yQw5mqImEwMHcau8Hb9CXnPQaTlz11cUCGhUZL64yG1GyAyMJddLMqfjiLwlZgvKfdC2yo896ykJVwW8Xne9++3BjCqj9xsNEdeENjtWVda5UTFIw149B2ygMnCp/4Fcn/nAV1YYOX1YLNxEJkiHwwdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
     $content = file_get_contents('php://input');
     $arrayJson = json_decode($content, true);
@@ -44,6 +66,8 @@ $COUNTN++;
 
 
     print_r($benz1);
+    
+    print_r(order_array_num ($benz1, 4, "ASC"));
 
 }
    
