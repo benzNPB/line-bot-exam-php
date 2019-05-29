d0a11c2c0d774ef91b043a30c2c4a8ea6e6ef112
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-05-29 13:53:59 +0700

    Update locatetest.php

--- locatetest.php
+++ locatetest.php
@@ -17,7 +17,26 @@ $locate = array();
         $sql = "SELECT no,name,lati,lng FROM contest order by no desc limit 0,5";
         $result = $conn->query($sql);
           
+ if ($result->num_rows > 0) {
+          while($row = $result->fetch_assoc() ){
+                  $lati1 = $row["lati"];
+                  $lng1 = $row["lng"];
+                     $deltaLat1 = deg2rad($lati1 - $latu);
+                     $deltaLong1 = deg2rad($lng1 - $longu);
+                   
+                    $a1 = sin($deltaLat1/2) * sin($deltaLat1/2) + cos(deg2rad($lati1)) * cos(deg2rad($latu)) * sin($deltaLong1/2) * sin($deltaLong1/2);
+                    $c1 = 2 * atan2(sqrt($a1), sqrt(1-$a1));
+                    $dis = $R * $c1;
 
+                    $benz1[$COUNTN] = array('name' => $row["name"] , 'lati' => $row["lati"] , 'lng' => $row["lng"] , 'dis' => $dis);
+                   // $benz1[$COUNTN][0] = $row["name"];
+                    //$benz1[$COUNTN][1] = $row["lati"];
+                   // $benz1[$COUNTN][2] = $row["lng"];
+                   // $benz1[$COUNTN][3] = $dis;
+$COUNTN++;
+          }
+
+}
    
 ?>
 
