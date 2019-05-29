abc86567d90f378d37b7504880c042fc4c3dff0c
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-05-29 16:25:22 +0700

    Update locationandtext5.php

--- locationandtext5.php
+++ locationandtext5.php
@@ -1,9 +1,28 @@
-
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-   require "dbconnection.php";
+    require "dbconnection.php";
+   
+function order_array_num ($array, $key, $order = "ASC") 
+{ 
+  $tmp = array(); 
+  foreach($array as $akey => $array2) 
+  { 
+    $tmp[$akey] = $array2[$key]; 
+  } 
+  
+  if($order == "DESC") 
+  {arsort($tmp , SORT_NUMERIC );} 
+  else 
+  {asort($tmp , SORT_NUMERIC );} 
+
+  $tmp2 = array();        
+  foreach($tmp as $key => $value) 
+  { 
+    $tmp2[$key] = $array[$key]; 
+  }        
+  
+  return $tmp2; 
+} 
+
     $accessToken = "yQw5mqImEwMHcau8Hb9CXnPQaTlz11cUCGhUZL64yG1GyAyMJddLMqfjiLwlZgvKfdC2yo896ykJVwW8Xne9++3BjCqj9xsNEdeENjtWVda5UTFIw149B2ygMnCp/4Fcn/nAV1YYOX1YLNxEJkiHwwdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
     $content = file_get_contents('php://input');
     $arrayJson = json_decode($content, true);
@@ -13,19 +32,16 @@ error_reporting(E_ALL);
     $text = $arrayJson['events'][0]['message']['text'];
     $location = $arrayJson['events'][0]['message']['location'];
     $message = $arrayJson['events'][0]['message']['text'];
-
-
-
-
-    $R = 6371;
+$R = 6371;
 $benz1 = array();
 $locate = array();
+  
+
       if($message == $location)
     {
    $latu = $arrayJson['events'][0]['message']['latitude'];//users location 
    $longu = $arrayJson['events'][0]['message']['longitude'];
-
-$COUNTN=0;       
+       $COUNTN=0;       
         $sql = "SELECT no,name,lati,lng FROM contest order by no desc limit 0,5";
         $result = $conn->query($sql);
           
@@ -41,28 +57,22 @@ $COUNTN=0;
                     $dis = $R * $c1;
 
                     $benz1[] = array('name' => $row["name"] , 'lati' => $row["lati"] , 'lng' => $row["lng"] , 'dis' => $dis);
-
+   
 $COUNTN++;
           }
 
   $mybenz = order_array_num ($benz1, "dis", "ASC");
    
    /////////////////////////// use
-   $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = We recommend 3 place for you;
-        replyMsg($arrayHeader,$arrayPostData);
    
+   $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+   $arrayPostData['messages'][0]['type'] = "text";
+   $arrayPostData['messages'][0]['text'] = We recommend 3 place for you;
+   replyMsg($arrayHeader,$arrayPostData);
+   }
+ }
 
-
-
-}
-
-}
-
-
-
-   else if($message == $text)
+    else if($message == $text)
     {
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
@@ -97,28 +107,7 @@ $COUNTN++;
       $result = curl_exec($ch);
       curl_close ($ch);
    }
-
-   function order_array_num ($array, $key, $order = "ASC") 
-{ 
-  $tmp = array(); 
-  foreach($array as $akey => $array2) 
-  { 
-    $tmp[$akey] = $array2[$key]; 
-  } 
-  
-  if($order == "DESC") 
-  {arsort($tmp , SORT_NUMERIC );} 
-  else 
-  {asort($tmp , SORT_NUMERIC );} 
-
-  $tmp2 = array();        
-  foreach($tmp as $key => $value) 
-  { 
-    $tmp2[$key] = $array[$key]; 
-  }        
-  
-  return $tmp2; 
-} 
-
+ 
+        
+   exit;
 ?>
-
