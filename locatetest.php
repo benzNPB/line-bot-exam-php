1ffa340ba02edda8a25bc4a09004ea3a0af7a146
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-05-29 13:50:39 +0700

    Update locatetest.php

--- locatetest.php
+++ locatetest.php
@@ -4,6 +4,20 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
    require "dbconnection.php";
+$accessToken = "yQw5mqImEwMHcau8Hb9CXnPQaTlz11cUCGhUZL64yG1GyAyMJddLMqfjiLwlZgvKfdC2yo896ykJVwW8Xne9++3BjCqj9xsNEdeENjtWVda5UTFIw149B2ygMnCp/4Fcn/nAV1YYOX1YLNxEJkiHwwdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+    $content = file_get_contents('php://input');
+    $arrayJson = json_decode($content, true);
+    $arrayHeader = array();
+    $R = 6371;
+    $benz1 = array();
+$locate = array();
+  $latu = 10.000000;
+   $longu = 111.111111;
+        $COUNTN=0;       
+        $sql = "SELECT no,name,lati,lng FROM contest order by no desc limit 0,5";
+        $result = $conn->query($sql);
+          
+}
    
 ?>
 
