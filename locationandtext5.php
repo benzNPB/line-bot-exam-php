f73aa9c962b4bfcbe3b2f8bd59d1eea917996175
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-05-29 19:39:26 +0700

    Update locationandtext5.php

--- locationandtext5.php
+++ locationandtext5.php
@@ -6,35 +6,41 @@ error_reporting(E_ALL);
    require "dbconnection.php";
 function order_array_num ($array, $key, $order = "ASC") 
 { 
-	$tmp = array(); 
-	foreach($array as $akey => $array2) 
-	{ 
-		$tmp[$akey] = $array2[$key]; 
-	} 
-	
-	if($order == "DESC") 
-	{arsort($tmp , SORT_NUMERIC );} 
-	else 
-	{asort($tmp , SORT_NUMERIC );} 
+  $tmp = array(); 
+  foreach($array as $akey => $array2) 
+  { 
+    $tmp[$akey] = $array2[$key]; 
+  } 
+  
+  if($order == "DESC") 
+  {arsort($tmp , SORT_NUMERIC );} 
+  else 
+  {asort($tmp , SORT_NUMERIC );} 
 
-	$tmp2 = array();        
-	foreach($tmp as $key => $value) 
-	{ 
-		$tmp2[$key] = $array[$key]; 
-	}        
-	
-	return $tmp2; 
+  $tmp2 = array();        
+  foreach($tmp as $key => $value) 
+  { 
+    $tmp2[$key] = $array[$key]; 
+  }        
+  
+  return $tmp2; 
 } 
 
 $accessToken = "yQw5mqImEwMHcau8Hb9CXnPQaTlz11cUCGhUZL64yG1GyAyMJddLMqfjiLwlZgvKfdC2yo896ykJVwW8Xne9++3BjCqj9xsNEdeENjtWVda5UTFIw149B2ygMnCp/4Fcn/nAV1YYOX1YLNxEJkiHwwdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
     $content = file_get_contents('php://input');
     $arrayJson = json_decode($content, true);
     $arrayHeader = array();
-    $R = 6371;
-    $benz1 = array();
+    $arrayHeader[] = "Content-Type: application/json";
+    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+    $text = $arrayJson['events'][0]['message']['text'];
+    $location = $arrayJson['events'][0]['message']['location'];
+    $message = $arrayJson['events'][0]['message']['text'];
+
+$R = 6371;
+$benz1 = array();
 $locate = array();
-  $latu = 10.000000;
-   $longu = 111.111111;
+$latu = 10.000000;
+$longu = 111.111111;
         $COUNTN=0;       
         $sql = "SELECT no,name,lati,lng FROM contest order by no desc limit 0,5";
         $result = $conn->query($sql);
@@ -55,14 +61,14 @@ $locate = array();
 $COUNTN++;
           }
 
-	$mybenz = order_array_num ($benz1, "dis", "ASC");
-	 
-	 /////////////////////////// use
+  $mybenz = order_array_num ($benz1, "dis", "ASC");
+   
+   /////////////////////////// use
 
-	 for($i=0;$i<=3;$i++){
-		 echo $mybenz[$i]["name"];
+   for($i=0;$i<=3;$i++){
+     echo $mybenz[$i]["name"];
 
-	 }
+   }
 
 }
    
