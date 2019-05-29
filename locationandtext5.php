d808ea4ddc12e1b8381a1e6ce1cbf20c698b6a18
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-05-29 16:37:13 +0700

    Update locationandtext5.php

--- locationandtext5.php
+++ locationandtext5.php
@@ -14,14 +14,14 @@ error_reporting(E_ALL);
     $message = $arrayJson['events'][0]['message']['text'];
     $R = 6371;
     $benz1 = array();
-$locate = array();
-  $latu = 10.000000;
-   $longu = 111.111111;
-        $COUNTN=0;       
-        $sql = "SELECT no,name,lati,lng FROM contest order by no desc limit 0,5";
-        $result = $conn->query($sql);
-          
- if ($result->num_rows > 0) {
+    $locate = array();
+
+
+      if($message == $location)
+    {
+         $latu = $arrayJson['events'][0]['message']['latitude'];//users location 
+         $longu = $arrayJson['events'][0]['message']['longitude'];
+          if ($result->num_rows > 0) {
           while($row = $result->fetch_assoc() ){
                   $lati1 = $row["lati"];
                   $lng1 = $row["lng"];
@@ -31,20 +31,83 @@ $locate = array();
                     $a1 = sin($deltaLat1/2) * sin($deltaLat1/2) + cos(deg2rad($lati1)) * cos(deg2rad($latu)) * sin($deltaLong1/2) * sin($deltaLong1/2);
                     $c1 = 2 * atan2(sqrt($a1), sqrt(1-$a1));
                     $dis = $R * $c1;
+
                     $benz1[] = array('name' => $row["name"] , 'lati' => $row["lati"] , 'lng' => $row["lng"] , 'dis' => $dis);
 
-$COUNTN++;
-          }
+                    $COUNTN++;
+                                            }
 
   $mybenz = order_array_num ($benz1, "dis", "ASC");
    
    /////////////////////////// use
    
-   for($i=0;$i<=3;$i++){
-     echo $mybenz[$i]["name"];
-      
-     
-   }
+   $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+   $arrayPostData['messages'][0]['type'] = "text";
+   $arrayPostData['messages'][0]['text'] = We recommend 3 place for you;
+   replyMsg($arrayHeader,$arrayPostData);
+   
+
 }
+ }
+//////////////////////////////////////////////////////////////
+   
+else if($message == $text)
+    {
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = $message.":".$text;
+        replyMsg($arrayHeader,$arrayPostData);
+    }
+    //////////////////////////////////////////////////////////
+
+      function replyMsg($arrayHeader,$arrayPostData){
+        $strUrl = "https://api.line.me/v2/bot/message/reply";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+    }
+    
+     function pushMsg($arrayHeader,$arrayPostData){
+      $strUrl = "https://api.line.me/v2/bot/message/push";
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,$strUrl);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      $result = curl_exec($ch);
+      curl_close ($ch);
+   }
+   function order_array_num ($array, $key, $order = "ASC") 
+{ 
+  $tmp = array(); 
+  foreach($array as $akey => $array2) 
+  { 
+    $tmp[$akey] = $array2[$key]; 
+  } 
+  
+  if($order == "DESC") 
+  {arsort($tmp , SORT_NUMERIC );} 
+  else 
+  {asort($tmp , SORT_NUMERIC );} 
+
+  $tmp2 = array();        
+  foreach($tmp as $key => $value) 
+  { 
+    $tmp2[$key] = $array[$key]; 
+  }        
+  
+  return $tmp2; 
+} 
+
    
 ?>
