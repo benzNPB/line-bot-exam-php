12261ff0aec33f1f2e9983dfc8e53f60b5b5454b
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-05 14:53:30 +0700

    Update locationandtext7.php

--- locationandtext7.php
+++ locationandtext7.php
@@ -59,7 +59,7 @@ $COUNTN++;
         $arrayPostData['messages'][2]['latitude'] =  $mybenz[2]["lati"];
         $arrayPostData['messages'][2]['longitude'] =  $mybenz[2]["lng"];
 
-       $query = "INSERT INTO user VALUES ('test', 'Benz', 'lati','lng')";
+       $query = "INSERT INTO user(iduser,name,lati,lng) VALUES ('test', 'Benz', 'lati' , 'lng')";
        mysqli_query($conn,$query );
    
         $arrayPostData['messages'][3]['type'] = "text";
