2ab1e322af68c53074ab800fa212b0a69c30f65c
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-05 11:28:02 +0700

    Add files via upload

--- locationandtext6.php
+++ locationandtext6.php
@@ -88,22 +88,10 @@ $COUNTN++;
 }
    else if($message == $text)
     {
+        $textu = $event['source']['userId'];
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-
-date_default_timezone_set(“Asia/Bangkok”);
-$date = date(“Y-m-d”);
-$time = date(“H:i:s”);
-$json = file_get_contents(‘php://input’);
-$request = json_decode($json, true);
-$queryText = $request[“queryResult”][“queryText”];
-$userId = $request[‘originalDetectIntentRequest’][‘payload’][‘data’][‘source’][‘userId’];
-$myfile = fopen(“log$date.txt”, “a”) or die(“Unable to open file!”);
-$log = $date.”-”.$time.”\t”.$userId.”\t”.$queryText.”\n”;
-fwrite($myfile,$log);
-
-
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = $message.":".$myfile;
+        $arrayPostData['messages'][0]['text'] = $textu.":".$message;
         replyMsg($arrayHeader,$arrayPostData);
     }
          function replyMsg($arrayHeader,$arrayPostData){
