41be6815621303c076632f861729e934403b44b3
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-05 11:40:24 +0700

    Update locationandtext6.php

--- locationandtext6.php
+++ locationandtext6.php
@@ -92,7 +92,7 @@ $COUNTN++;
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
         $arrayPostData['messages'][0]['text'] = $textu.":".$message;
-        replyMsg($arrayHeader,$arrayPostData);
+        getuserid($arrayHeader,$arrayPostData);
     }
          function replyMsg($arrayHeader,$arrayPostData){
         $strUrl = "https://api.line.me/v2/bot/message/reply";
@@ -121,6 +121,21 @@ $COUNTN++;
       $result = curl_exec($ch);
       curl_close ($ch);
    }
+
+        function getuserid($arrayHeader,$arrayPostData){
+      $strUrl = "https://api.line.me/v2/bot/profile/{userId}";
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,$strUrl);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      $result = curl_exec($ch);
+      curl_close ($ch);
+   }
+
 
  
         
