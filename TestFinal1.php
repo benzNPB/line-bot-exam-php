e148f1f8a5095ceb54b0acd681efadd54a448580
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-11 20:56:58 +0700

    Add files via upload

---
+++ TestFinal1.php
@@ -0,0 +1,151 @@
+<?php
+    require "dbconnection.php";
+
+    $accessToken = "yQw5mqImEwMHcau8Hb9CXnPQaTlz11cUCGhUZL64yG1GyAyMJddLMqfjiLwlZgvKfdC2yo896ykJVwW8Xne9++3BjCqj9xsNEdeENjtWVda5UTFIw149B2ygMnCp/4Fcn/nAV1YYOX1YLNxEJkiHwwdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+    $content = file_get_contents('php://input');
+    $arrayJson = json_decode($content, true);
+    $arrayHeader = array();
+    $arrayHeader[] = "Content-Type: application/json";
+    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+    $text = $arrayJson['events'][0]['message']['text'];
+    $location = $arrayJson['events'][0]['message']['location'];
+    $message = $arrayJson['events'][0]['message']['text'];
+         
+$R = 6371;
+$benz1 = array();
+$locate = array();
+        $COUNTN=0;       
+        $sql = "SELECT no,name,lati,lng FROM contest";
+        $result = $conn->query($sql);
+          if($message == "Evacuation Point")
+    {        
+              $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "please send your location to bot and bot will send nearest evacution point to you";
+        replyMsg($arrayHeader,$arrayPostData);
+
+          if($message == $location)
+    {
+   $latu = $arrayJson['events'][0]['message']['latitude'];//users location 
+   $longu = $arrayJson['events'][0]['message']['longitude'];
+   $userid = $arrayJson['events'][0]['source']['userId'];
+ if ($result->num_rows > 0) {
+          while($row = $result->fetch_assoc() ){
+                  $lati1 = $row["lati"];
+                  $lng1 = $row["lng"];
+                     $deltaLat1 = deg2rad($lati1 - $latu);
+                     $deltaLong1 = deg2rad($lng1 - $longu);
+                   
+                    $a1 = sin($deltaLat1/2) * sin($deltaLat1/2) + cos(deg2rad($lati1)) * cos(deg2rad($latu)) * sin($deltaLong1/2) * sin($deltaLong1/2);
+                    $c1 = 2 * atan2(sqrt($a1), sqrt(1-$a1));
+                    $dis = $R * $c1;
+                    $benz1[] = array('name' => $row["name"] , 'lati' => $row["lati"] , 'lng' => $row["lng"] , 'dis' => $dis);
+ 
+$COUNTN++;
+          }
+  $mybenz = order_array_num ($benz1, "dis", "ASC");
+   
+///////////////////////////////
+
+ if($idu = U434d98c2ea737a9af2b3401a2c0abcbb){
+ 	$uid = 'Benz';
+ }
+
+   /////////////////////////// use
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "Here is your nearest Evacuation point";
+        $arrayPostData['messages'][1]['type'] = "location";
+        $arrayPostData['messages'][1]['title'] = $mybenz[0]["name"];
+        $arrayPostData['messages'][1]['address'] =   $mybenz[0]["lati"].",".$mybenz[0]["lng"];
+        $arrayPostData['messages'][1]['latitude'] =  $mybenz[0]["lati"];
+        $arrayPostData['messages'][1]['longitude'] =  $mybenz[0]["lng"];
+        $arrayPostData['messages'][2]['type'] = "location";
+        $arrayPostData['messages'][2]['title'] = $mybenz[1]["name"];
+        $arrayPostData['messages'][2]['address'] =   $mybenz[1]["lati"].",".$mybenz[1]["lng"];
+        $arrayPostData['messages'][2]['latitude'] =  $mybenz[1]["lati"];
+        $arrayPostData['messages'][2]['longitude'] =  $mybenz[1]["lng"];
+        $arrayPostData['messages'][3]['type'] = "location";
+        $arrayPostData['messages'][3]['title'] = $mybenz[2]["name"];
+        $arrayPostData['messages'][3]['address'] =   $mybenz[2]["lati"].",".$mybenz[2]["lng"];
+        $arrayPostData['messages'][3]['latitude'] =  $mybenz[2]["lati"];
+        $arrayPostData['messages'][3]['longitude'] =  $mybenz[2]["lng"];
+
+       $query = "INSERT INTO user(name,lati,lng,iduserlink) VALUES ('".$uid."', '".$latu."', '".$longu."','".$arrayJson['events'][0]['source']['userId']."' )";
+       mysqli_query($conn,$query );
+   
+
+        replyMsg($arrayHeader,$arrayPostData);
+}
+}
+}
+   else if($message == $text)
+    {
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = $arrayJson['events'][0]['source']['userId'];
+        replyMsg($arrayHeader,$arrayPostData);
+    }
+         function replyMsg($arrayHeader,$arrayPostData){
+        $strUrl = "https://api.line.me/v2/bot/message/reply";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+    }
+    
+     function pushMsg($arrayHeader,$arrayPostData){
+      $strUrl = "https://api.line.me/v2/bot/message/push";
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,$strUrl);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      $result = curl_exec($ch);
+      curl_close ($ch);
+   }
+        function getuserid($arrayHeader,$arrayPostData){
+      $strUrl = "https://api.line.me/v2/bot/profile/{userId}";
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,$strUrl);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      $result = curl_exec($ch);
+      curl_close ($ch);
+   }
+ 
+       function order_array_num ($array, $key, $order = "ASC") 
+{ 
+  $tmp = array(); 
+  foreach($array as $akey => $array2) 
+  { 
+    $tmp[$akey] = $array2[$key]; 
+  } 
+  
+  if($order == "DESC") 
+  {arsort($tmp , SORT_NUMERIC );} 
+  else 
+  {asort($tmp , SORT_NUMERIC );} 
+  $tmp2 = array();        
+  $i = 0;
+  foreach($tmp as $key => $value) {
+    $tmp2[$i] = $array[$key];
+    $i++;
+  }
+  return $tmp2; 
+}      
+   exit;
+?>
