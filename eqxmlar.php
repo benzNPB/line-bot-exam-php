f75236bedde2984fde4ddeaf045fc636cfcbda65
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-12 22:35:41 +0700

    Update eqxmlar.php

--- eqxmlar.php
+++ eqxmlar.php
@@ -1,5 +1,6 @@
 <?php
-$url = "http://geofon.gfz-potsdam.de/eqinfo/list.php?fmt=rss";
+//$url = "http://geofon.gfz-potsdam.de/eqinfo/list.php?fmt=rss";
+$url = "http://agora.ex.nii.ac.jp/earthquake/hypocenter/#xml";
 $xml1 = simplexml_load_file($url);
 $xml2 = $xml1->channel->item[0]->description;
 
