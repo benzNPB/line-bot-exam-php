d59ae5f28f75803902088e51a5f018bcda7dc57a
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-12 22:31:30 +0700

    Update eqxml1.php

--- eqxml1.php
+++ eqxml1.php
@@ -1,8 +1,10 @@
 <?php
-$url = "http://geofon.gfz-potsdam.de/eqinfo/list.php?fmt=rss";
+//$url = "http://geofon.gfz-potsdam.de/eqinfo/list.php?fmt=rss";
+$url = "http://agora.ex.nii.ac.jp/earthquake/hypocenter/#xml";
 $xml1 = simplexml_load_file($url);
 $xml2 = $xml1->channel->item[0]->description;
 $xml3 = $xml1->channel->item[0]->title;
 $xml = (explode(" ",$xml2));
-echo $xml3.':'.$xml[3].','.$xml[6];
+//echo $xml3.':'.$xml[3].','.$xml[6];
+echo $xml1;
 ?>
