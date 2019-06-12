2636369bb2d82a9631d2bc21c55e7edc99da3c13
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-12 13:02:20 +0700

    Update TestFinal2.php

--- TestFinal2.php
+++ TestFinal2.php
@@ -18,7 +18,8 @@
   $username = 'Benz';
  }
 
-       $query = "INSERT INTO command(iduserlink,username,command) VALUES ('".$arrayJson['events'][0]['source']['userId']."' , '".$username."', 'Evacuation')";
+              $currenttime = date("d-M-Y H:i:s")
+       $query = "INSERT INTO command(iduserlink,username,command,time) VALUES ('".$arrayJson['events'][0]['source']['userId']."' , '".$username."', 'Evacuation', '".$currenttime."')";
        mysqli_query($conn,$query );
    
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
