0bba4d3c6ce7b18c730324dc59b13d16ca7dc216
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-12 12:49:27 +0700

    Add files via upload

---
+++ TestFinal2.php
@@ -0,0 +1,81 @@
+<?php
+    require "dbconnection.php";
+
+    $accessToken = "yQw5mqImEwMHcau8Hb9CXnPQaTlz11cUCGhUZL64yG1GyAyMJddLMqfjiLwlZgvKfdC2yo896ykJVwW8Xne9++3BjCqj9xsNEdeENjtWVda5UTFIw149B2ygMnCp/4Fcn/nAV1YYOX1YLNxEJkiHwwdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+    $content = file_get_contents('php://input');
+    $arrayJson = json_decode($content, true);
+    $arrayHeader = array();
+    $arrayHeader[] = "Content-Type: application/json";
+    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+    $text = $arrayJson['events'][0]['message']['text'];
+    $location = $arrayJson['events'][0]['message']['location'];
+    $message = $arrayJson['events'][0]['message']['text'];
+         
+          if($message == "Evacuation Point")
+    {        
+   $userid = $arrayJson['events'][0]['source']['userId'];
+       if($userid = U434d98c2ea737a9af2b3401a2c0abcbb){
+  $username = 'Benz';
+ }
+
+       $query = "INSERT INTO command(iduserlink,username,command) VALUES ('".$arrayJson['events'][0]['source']['userId']."' , '".$username."', "Evacuation")";
+       mysqli_query($conn,$query );
+   
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "please send your location to bot and bot will send nearest evacution point to you";
+        replyMsg($arrayHeader,$arrayPostData);
+
+}
+
+         function replyMsg($arrayHeader,$arrayPostData){
+        $strUrl = "https://api.line.me/v2/bot/message/reply";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+    }
+    
+     function pushMsg($arrayHeader,$arrayPostData){
+      $strUrl = "https://api.line.me/v2/bot/message/push";
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,$strUrl);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      $result = curl_exec($ch);
+      curl_close ($ch);
+   }
+
+ 
+       function order_array_num ($array, $key, $order = "ASC") 
+{ 
+  $tmp = array(); 
+  foreach($array as $akey => $array2) 
+  { 
+    $tmp[$akey] = $array2[$key]; 
+  } 
+  
+  if($order == "DESC") 
+  {arsort($tmp , SORT_NUMERIC );} 
+  else 
+  {asort($tmp , SORT_NUMERIC );} 
+  $tmp2 = array();        
+  $i = 0;
+  foreach($tmp as $key => $value) {
+    $tmp2[$i] = $array[$key];
+    $i++;
+  }
+  return $tmp2; 
+}      
+   exit;
+?>
