d2248302827472f4aa5479cf968fc169e8d86cda
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-18 21:52:18 +0700

    Update eqxmlar.php

--- eqxmlar.php
+++ eqxmlar.php
@@ -4,7 +4,7 @@ $url = "http://geofon.gfz-potsdam.de/eqinfo/list.php?fmt=rss";
 $xml1 = simplexml_load_file($url);
 $xml2 = $xml1->channel->item[0]->title;
 $xml = (explode(" ",$xml2));
-print_r ($xml2);
-echo $xml2.':'.$xml[3].','.$xml[6];
+
+echo $xml;
 
 ?>
