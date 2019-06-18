a718dbb85b77620936b7171721da912548a983e8
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-18 21:30:34 +0700

    Update eqxmlar.php

--- eqxmlar.php
+++ eqxmlar.php
@@ -3,7 +3,7 @@ $url = "http://geofon.gfz-potsdam.de/eqinfo/list.php?fmt=rss";
 //$url = "https://www.jma.go.jp/en/quake/quake_singendo_index.html";
 $xml1 = simplexml_load_file($url);
 $xml2 = $xml1->channel->item[0]->description;
-
+$xml = (explode(" ",$xml2));
 print_r ($xml2);
 
 ?>
