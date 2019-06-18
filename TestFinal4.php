25517e0c01259f95b2ef5e66fa163e0f92aca8c8
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-18 15:54:25 +0700

    Update TestFinal4.php

--- TestFinal4.php
+++ TestFinal4.php
@@ -151,13 +151,13 @@ $COUNTN++;
         $arrayPostData['messages'][3]['address'] =   $mybenz[2]["lati"].",".$mybenz[2]["lng"];
         $arrayPostData['messages'][3]['latitude'] =  $mybenz[2]["lati"];
         $arrayPostData['messages'][3]['longitude'] =  $mybenz[2]["lng"];
-      if($mybenz[3]["dis"]<1) {
+      if($mybenz[$j]["dis"]<1) {
       $link[$j] = "https://www.google.com/search?hl=th&ei=mI0IXf2aHPmVr7wP5-CroAo&q=".$mybenz[$j]["lati"]."%2C".$mybenz[$j]["lng"];
-  //  $link = $link[$j].",".$link;
+      $link = $link[$j].",".$link;
   //  $arrayPostData['messages'][4]['type'] = "text";
   //  $arrayPostData['messages'][4]['text'] = $link;
    //   $nlink = $link[$j]
- //   $j++;
+    $j++;
   }
 
         replyMsg($arrayHeader,$arrayPostData);
