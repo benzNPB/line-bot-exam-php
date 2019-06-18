aebc5532ffeace89d7f0ce1f53e49098a6c26c2f
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-18 13:43:17 +0700

    Update TestFinal4.php

--- TestFinal4.php
+++ TestFinal4.php
@@ -85,7 +85,7 @@ ini_set('display_errors', 1);
 $COUNTN++;
           }
   $mybenz = order_array_num ($benz1, "dis", "ASC");
-   
+                $j =3;
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
         $arrayPostData['messages'][0]['text'] = "Here is your nearest Evacuation point";
@@ -105,7 +105,7 @@ $COUNTN++;
         $arrayPostData['messages'][3]['latitude'] =  $mybenz[2]["lati"];
         $arrayPostData['messages'][3]['longitude'] =  $mybenz[2]["lng"];
         $arrayPostData['messages'][4]['type'] = "text";
-        $arrayPostData['messages'][4]['text'] = "https://www.google.com/maps/@".$mybenz[3]["lati"].",".$mybenz[3]["lng"].",15z";
+        $arrayPostData['messages'][4]['text'] = "https://www.google.com/maps/@".$mybenz[$j]["lati"].",".$mybenz[$j]["lng"].",15z";
         replyMsg($arrayHeader,$arrayPostData);
 }
 
