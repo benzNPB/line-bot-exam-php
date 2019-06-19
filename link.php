a80694a3bff53e00b54d38bdff6fe56a3d7dcbb0
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-19 18:44:12 +0700

    Update link.php

--- link.php
+++ link.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-$url = 'https://www.bousai.pref.kanagawa.jp/?fbclid=IwAR2o4skyEIe50B9UMnFxumz7M0N0vSEVBoZLtuYGTKRq8QKsimFugQLz_4I';
+$url = 'http://www.jma.go.jp/jp/quake/quake_local_index.html';
 //file_get_contents($url)
 echo file_get_contents($url);
    
