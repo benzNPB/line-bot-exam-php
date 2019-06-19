0f52f1b2739d4dfc36abdd2c1feb2b41c0ce6b4a
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-19 11:41:42 +0700

    Update eqxmlar.php

--- eqxmlar.php
+++ eqxmlar.php
@@ -5,16 +5,14 @@
            $xml1 = simplexml_load_file($url);
            $xml2 = $xml1->channel->item[0]->description;
            $xml3 = $xml1->channel->item[1]->title;
-           $xml = (explode(" ",$xml2));
+           $xml = (explode(" ",$xml3));
    echo '<pre>';
 print_r ($xml3[0]);
 	echo '</pre>';
    echo '<pre>';
 print_r ($xml2[0]);
 	echo '</pre>';
-   echo '<pre>';
-print_r ($xml2[1]);
-	echo '</pre>';
+
    echo '<pre>';
 print_r ($xml);
 	echo '</pre>';
