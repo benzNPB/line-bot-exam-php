14b140885ad5bafa7e33e2f54c89723a2d5bcd76
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-19 19:09:22 +0700

    Update link.php

--- link.php
+++ link.php
@@ -2,7 +2,11 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 $url = 'http://www.jma.go.jp/jp/quake/quake_local_index.html';
-//file_get_contents($url)
+$link = file_get_contents($url);
 echo file_get_contents($url);
+
+$o = strpos($link,"情報発表日時" );
+$s = strpos($link,"分" );
+echo substr($xml->channel->item[0]->title,$o ,$s-$o);
    
 ?>
