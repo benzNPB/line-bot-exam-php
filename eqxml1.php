8ab3cbab3907abce31cba0f82de06a9794e086df
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-26 17:11:32 +0700

    Update eqxml1.php

--- eqxml1.php
+++ eqxml1.php
@@ -1,13 +1,14 @@
 <?php
            $url = "http://www.gdacs.org/xml/rss.xml";
            $xml = simplexml_load_file($url);
+           $xml1 = simplexml_load_string($url);
            $xmlt = $xml->channel->item[0]->description;
            $xmlet = (explode(" ",$xmlt));
                echo '<pre>';
 echo $xmlt;
                echo '</pre>';
                echo '<pre>';
-echo $xmlet;
+echo $xml1;
                echo '</pre>';
 
 ?>
