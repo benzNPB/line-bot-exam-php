e6f39e79e6ba80202e79599a78e42f0a8a1fb4d6
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-26 17:27:48 +0700

    Update eqxml1.php

--- eqxml1.php
+++ eqxml1.php
@@ -1,17 +1,16 @@
 <?php
            $url = "http://www.gdacs.org/xml/rss.xml";
            $xml = simplexml_load_file($url);
-
            $xmlt = $xml->channel->item[0]->description;
 
 
-foreach($xml->channel->children() as $child)
+foreach($xml->channel->item() as $child)
   {
 
     ///
       if($childs->getName()=="item"){
 
-        foreach($childs->children('geo', TRUE) as $items)
+        foreach($childs->item('geo', TRUE) as $items)
           {
             echo $items->lat->getName() . ": " . $items->lat."<br>";
             echo $items->long->getName() . ": " . $items->long."<br>";
