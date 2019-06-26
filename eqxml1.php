a7d7c6514d4b94e8c237d88f405b1d3850ee481a
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-26 16:44:51 +0700

    Update eqxml1.php

--- eqxml1.php
+++ eqxml1.php
@@ -3,6 +3,11 @@
            $xml = simplexml_load_file($url);
            $xmlt = $xml->channel->item[0]->description;
            $xmlet = (explode(" ",$xmlt));
+               echo '<pre>';
 echo $xmlt;
+               echo '</pre>';
+               echo '<pre>';
 echo $xmlet;
+               echo '</pre>';
+
 ?>
