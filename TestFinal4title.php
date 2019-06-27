1b70ffe14e9eb867feaa2f6dabc2e12b7207168d
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-06-27 11:21:27 +0700

    Update TestFinal4title.php

--- TestFinal4title.php
+++ TestFinal4title.php
@@ -136,6 +136,7 @@ $COUNTN++;
         $arrayPostData['messages'][0]['type'] = "text";
         $arrayPostData['messages'][0]['text'] = $address;
         replyMsg($arrayHeader,$arrayPostData);
+         }
             ///////////////////////////////////////////////////////////////////////////////
               else if($row_command["Command"]=="People"){
         $sql = "SELECT name,lati,lng,iduserlink FROM user ";
