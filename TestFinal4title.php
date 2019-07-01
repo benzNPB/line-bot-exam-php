ada6c6b9f83a746540aeeea1340f169abae4da93
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-07-01 14:05:29 +0700

    Update TestFinal4title.php

--- TestFinal4title.php
+++ TestFinal4title.php
@@ -149,11 +149,11 @@ $COUNTN++;
         $arrayPostData['messages'][0]['text'] = "https://www.bousai.pref.kanagawa.jp/K_PUB_VF_DetailCity?cityid=a017F00000G5BtHQAV";
         replyMsg($arrayHeader,$arrayPostData);}
              }
-                 else if ($pos == 'Hadano-shi'){
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "https://www.bousai.pref.kanagawa.jp/K_PUB_VF_DetailCity?cityid=a017F00000G5BtHQAV";
-        replyMsg($arrayHeader,$arrayPostData);}
+   //              else if ($pos == 'Hadano-shi'){
+  //      $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+ //       $arrayPostData['messages'][0]['type'] = "text";
+   //     $arrayPostData['messages'][0]['text'] = "https://www.bousai.pref.kanagawa.jp/K_PUB_VF_DetailCity?cityid=a017F00000G5BtHQAV";
+  //      replyMsg($arrayHeader,$arrayPostData);}
              }
 
 
