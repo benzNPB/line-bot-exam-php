70a1d855ff97d62477fd33bb993cc2dc8e9ea5e3
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-07-03 12:36:28 +0700

    Update TestFinal4title.php

--- TestFinal4title.php
+++ TestFinal4title.php
@@ -132,7 +132,7 @@ $COUNTN++;
                         //////////////////////////////////EVACUATION LOCATION////////////////////////////////////////
          if($row_command["Command"]=="Location"){
         $address = $arrayJson['events'][0]['message']['address'];
-        $findme="Kanagawa-ken"; 
+        $findme=",Kanagawa-ken"; 
       // $tokens= explode(",", $address);  
         $pos = stristr($address, $findme);
         $spos = stristr($address,$findme, true);
