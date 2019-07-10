8fe8615141370de7aae965bfeb01695994c71eda
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-07-10 13:12:58 +0700

    Update TestFinal8.php

--- TestFinal8.php
+++ TestFinal8.php
@@ -41,13 +41,52 @@ ini_set('display_errors', 1);
         replyMsg($arrayHeader,$arrayPostData);
     }
          else if($message == "Test")
-    {        
+    {   
+                                     $actionBuilder = array(
+                            new MessageTemplateActionBuilder(
+                                'Message Template',// ข้อความแสดงในปุ่ม
+                                'This is Text' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
+                            ),
+                            new UriTemplateActionBuilder(
+                                'Uri Template', // ข้อความแสดงในปุ่ม
+                                'https://www.ninenik.com'
+                            ),
+                            new PostbackTemplateActionBuilder(
+                                'Postback', // ข้อความแสดงในปุ่ม
+                                http_build_query(array(
+                                    'action'=>'buy',
+                                    'item'=>100
+                                )), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
+                                'Postback Text'  // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
+                            ),      
+                        );
+                        $replyData = new TemplateMessageBuilder('Carousel',
+                            new CarouselTemplateBuilder(
+                                array(
+                                    new CarouselColumnTemplateBuilder(
+                                        'Title Carousel',
+                                        'Description Carousel',
+                                        'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/700',
+                                        $actionBuilder
+                                    ),
+                                    new CarouselColumnTemplateBuilder(
+                                        'Title Carousel',
+                                        'Description Carousel',
+                                        'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/700',
+                                        $actionBuilder
+                                    ),
+                                    new CarouselColumnTemplateBuilder(
+                                        'Title Carousel',
+                                        'Description Carousel',
+                                        'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/700',
+                                        $actionBuilder
+                                    ),                                          
+                                )
+                            )
+                        );
        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
        $arrayPostData['messages'][0]['type'] = "text";
        $arrayPostData['messages'][0]['text'] = $arrayJson['events'][0]['source']['userId'].",,,,,,".$userid;
-       if ($arrayJson['events'][0]['source']['userId'] == $userid)
-       $arrayPostData['messages'][1]['type'] = "text";
-       $arrayPostData['messages'][1]['text'] = "yes";
        replyMsg($arrayHeader,$arrayPostData);
     }
          else if($message == "DisasterInformation")
