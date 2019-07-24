28072c6e17ee0beb9dc252295dc1854d794187c2
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-07-24 14:18:31 +0700

    Update carousel.php

--- carousel.php
+++ carousel.php
@@ -55,7 +55,7 @@ use LINE\LINEBot\Event\FollowEvent;
         $columns  =  [];  // Add array of 5 carousel type columns 
         foreach  ( $lists  as  $list )  { 
                    // Create a button to give to carousel 
-    /*   $action  =  new  UriTemplateActionBuilder ( "Click and try " ,  / * Summary URL * /  ); 
+       $action  =  new  UriTemplateActionBuilder ( "Click and try " ,  / * Summary URL * /  ); 
         // Create carousel column 
          $column  =  new  CarouselColumnTemplateBuilder ( "Title (up to 40 characters)" ,  "Additional sentence" , "https://static.wixstatic.com/media/b46608_bd800c813dad44f69c121da3af790314.jpg/v1/fill/w_555,h_370,al_c,q_80,usm_0.66_1.00_0.01/b46608_bd800c813dad44f69c121da3af790314.jpg" ,  [ $action ]); 
         $columns []  =  $column ; */
