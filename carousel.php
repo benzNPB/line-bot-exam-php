ac0326f7d3e668ba0576bf111280876d5e255456
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-07-25 07:26:42 +0700

    Update carousel.php

--- carousel.php
+++ carousel.php
@@ -50,51 +50,11 @@ use LINE\LINEBot\Event\FollowEvent;
     {
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "carousel";
-        //////////////////////////////////////////////////////
-        /*                $actionBuilder = array(
-                            new MessageTemplateActionBuilder(
-                                'Message Template',// ข้อความแสดงในปุ่ม
-                                'This is Text' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
-                            ),
-                            new UriTemplateActionBuilder(
-                                'Uri Template', // ข้อความแสดงในปุ่ม
-                                'https://www.ninenik.com'
-                            ),
-                            new PostbackTemplateActionBuilder(
-                                'Postback', // ข้อความแสดงในปุ่ม
-                                http_build_query(array(
-                                    'action'=>'buy',
-                                    'item'=>100
-                                )), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
-                                'Postback Text'  // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
-                            ),      
-                        );
-                        $replyData = new TemplateMessageBuilder('Carousel',
-                            new CarouselTemplateBuilder(
-                                array(
-                                    new CarouselColumnTemplateBuilder(
-                                        'Title Carousel',
-                                        'Description Carousel',
-                                        'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/700',
-                                        $actionBuilder
-                                    ),
-                                    new CarouselColumnTemplateBuilder(
-                                        'Title Carousel',
-                                        'Description Carousel',
-                                        'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/700',
-                                        $actionBuilder
-                                    ),
-                                    new CarouselColumnTemplateBuilder(
-                                        'Title Carousel',
-                                        'Description Carousel',
-                                        'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/700',
-                                        $actionBuilder
-                                    ),                                          
-                                )
-                            )
-                        );*/
-        /////////////////////////////////////////////////////
-        $arrayPostData['messages'][0]['columns'] = "";
+        $benz2 = array();
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = $message.":".$text;
+        $benz2[] = array('text' => $arrayPostData['messages'][0]['text'] , 'test' => "Test");
+        $arrayPostData['messages'][0]['columns'] = $benz2;
           pushMsg($arrayHeader,$arrayPostData);
     }
       function replyMsg($arrayHeader,$arrayPostData){
