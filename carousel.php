3a6c402e3d7f67842d4b8d9c8d89e241f8223fe7
benzNPB <39670619+benzNPB@users.noreply.github.com>
2019-07-25 08:51:09 +0700

    Update carousel.php

--- carousel.php
+++ carousel.php
@@ -59,7 +59,7 @@ foreach ($lists as $list) {
     $columns[] = $column;
 }
 // カラムの配列を組み合わせてカルーセルを作成する
-$carousel = new CarouselTemplateBuilder($columns);
+$carousel = CarouselTemplateBuilder($columns);
 // カルーセルを追加してメッセージを作る
 /*$carousel_message = new TemplateMessageBuilder("メッセージのタイトル", $carousel);*/
         $arrayPostData['messages'][0]['columns'] = $column;
